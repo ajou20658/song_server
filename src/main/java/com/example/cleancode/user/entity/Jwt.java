eab85f1a2b3cf0cf2ae74939f011004d41a147ec
ajou20658 <kwy1379@ajou.ac.kr>
2023-08-07 15:05:51 +0900

    MemberEntity 오류

--- src/main/java/com/example/cleancode/user/entity/Jwt.java
+++ src/main/java/com/example/cleancode/user/entity/Jwt.java
@@ -13,6 +13,7 @@ public class Jwt {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "name", nullable = false, columnDefinition = "VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
     private String name;
     private String email;
     private String accessToken;
