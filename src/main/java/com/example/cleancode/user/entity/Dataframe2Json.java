768b0c16229741f174684c071858a852ea8b4af6
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-06 04:08:58 +0900

    Debug: Dataframe2Json 역직렬화 문제

--- src/main/java/com/example/cleancode/user/entity/Dataframe2Json.java
+++ src/main/java/com/example/cleancode/user/entity/Dataframe2Json.java
@@ -17,21 +17,21 @@ public class Dataframe2Json {
     private Integer f0_8;
     @JsonCreator
     public Dataframe2Json(
-            @JsonProperty("f0_1") Integer f0_1,
-            @JsonProperty("f0_2") Integer f0_2,
-            @JsonProperty("f0_3") Integer f0_3,
-            @JsonProperty("f0_4") Integer f0_4,
-            @JsonProperty("f0_5") Integer f0_5,
-            @JsonProperty("f0_6") Integer f0_6,
-            @JsonProperty("f0_7") Integer f0_7,
-            @JsonProperty("f0_8") Integer f0_8) {
-        this.f0_1 = f0_1;
-        this.f0_2 = f0_2;
-        this.f0_3 = f0_3;
-        this.f0_4 = f0_4;
-        this.f0_5 = f0_5;
-        this.f0_6 = f0_6;
-        this.f0_7 = f0_7;
-        this.f0_8 = f0_8;
+            @JsonProperty("f0_1") String f0_1,
+            @JsonProperty("f0_2") String f0_2,
+            @JsonProperty("f0_3") String f0_3,
+            @JsonProperty("f0_4") String f0_4,
+            @JsonProperty("f0_5") String f0_5,
+            @JsonProperty("f0_6") String f0_6,
+            @JsonProperty("f0_7") String f0_7,
+            @JsonProperty("f0_8") String f0_8) {
+        this.f0_1 = Integer.valueOf(f0_1);
+        this.f0_2 = Integer.valueOf(f0_2);
+        this.f0_3 = Integer.valueOf(f0_3);
+        this.f0_4 = Integer.valueOf(f0_4);
+        this.f0_5 = Integer.valueOf(f0_5);
+        this.f0_6 = Integer.valueOf(f0_6);
+        this.f0_7 = Integer.valueOf(f0_7);
+        this.f0_8 = Integer.valueOf(f0_8);
     }
 }
