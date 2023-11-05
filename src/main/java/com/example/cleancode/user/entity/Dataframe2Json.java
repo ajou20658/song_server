562bdc76f29a9b6b2d7e316f22a7b9b28f9d9e84
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-06 03:39:34 +0900

    Debug: Dataframe2Json 역직렬화 문제

--- src/main/java/com/example/cleancode/user/entity/Dataframe2Json.java
+++ src/main/java/com/example/cleancode/user/entity/Dataframe2Json.java
@@ -1,11 +1,13 @@
 package com.example.cleancode.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.GeneratedValue;
 import lombok.*;
 
 @Getter
 @Setter
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor
 public class Dataframe2Json {
     private Integer f0_1;
     private Integer f0_2;
@@ -15,4 +17,23 @@ public class Dataframe2Json {
     private Integer f0_6;
     private Integer f0_7;
     private Integer f0_8;
+    @JsonCreator
+    public Dataframe2Json(
+            @JsonProperty("f0_1") Integer f0_1,
+            @JsonProperty("f0_2") Integer f0_2,
+            @JsonProperty("f0_3") Integer f0_3,
+            @JsonProperty("f0_4") Integer f0_4,
+            @JsonProperty("f0_5") Integer f0_5,
+            @JsonProperty("f0_6") Integer f0_6,
+            @JsonProperty("f0_7") Integer f0_7,
+            @JsonProperty("f0_8") Integer f0_8) {
+        this.f0_1 = f0_1;
+        this.f0_2 = f0_2;
+        this.f0_3 = f0_3;
+        this.f0_4 = f0_4;
+        this.f0_5 = f0_5;
+        this.f0_6 = f0_6;
+        this.f0_7 = f0_7;
+        this.f0_8 = f0_8;
+    }
 }
