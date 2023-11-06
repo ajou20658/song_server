32ff27eb7d6ed037af627e4cfe5d2f6f47252bbf
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-06 15:10:32 +0900

    Fix: UserSongRepository awsUrl 변수 -> vocalUrl로 변환

--- src/main/java/com/example/cleancode/user/JpaRepository/UserSongRepository.java
+++ src/main/java/com/example/cleancode/user/JpaRepository/UserSongRepository.java
@@ -13,6 +13,6 @@ public interface UserSongRepository extends JpaRepository<UserSong,Long> {
     List<UserSong> findByUserId(Long userId);
     @Query("SELECT us FROM UserSong us WHERE us.song.id = :songId AND us.user.id = :userId")
     Optional<UserSong> findBySongIdAndUserId(@Param("songId") Long songId, @Param("userId") Long userId);
-    Optional<UserSong> findByAwsUrl(String awsUrl);
+    Optional<UserSong> findByVocalUrl(String vocalUrl);
     Optional<UserSong> findByOriginUrl(String originUrl);
 }
