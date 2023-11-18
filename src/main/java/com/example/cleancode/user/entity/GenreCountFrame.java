0fdfe25140a840cdc83c18623baf3093bcb5631b
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-19 01:20:41 +0900

    Feat: 장르카운트 디폴트 0으로 설정

--- src/main/java/com/example/cleancode/user/entity/GenreCountFrame.java
+++ src/main/java/com/example/cleancode/user/entity/GenreCountFrame.java
@@ -3,6 +3,7 @@ package com.example.cleancode.user.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -11,30 +12,43 @@ import lombok.Setter;
 @Getter
 @Setter
 public class GenreCountFrame {
+    @Builder.Default
     @Column(name = "J-POP")
-    private Integer a;
+    private Integer a=0;
+    @Builder.Default
     @Column(name = "POP")
-    private Integer b;
+    private Integer b=0;
+    @Builder.Default
     @Column(name = "R&B/Soul")
-    private Integer c;
+    private Integer c=0;
+    @Builder.Default
     @Column(name = "국내드라마")
-    private Integer d;
+    private Integer d=0;
+    @Builder.Default
     @Column(name = "댄스")
-    private Integer e;
+    private Integer e=0;
+    @Builder.Default
     @Column(name = "랩/힙합")
-    private Integer f;
+    private Integer f=0;
+    @Builder.Default
     @Column(name = "록/메탈")
-    private Integer g;
+    private Integer g=0;
+    @Builder.Default
     @Column(name = "발라드")
-    private Integer h;
+    private Integer h=0;
+    @Builder.Default
     @Column(name = "성인가요/트로트")
-    private Integer i;
+    private Integer i=0;
+    @Builder.Default
     @Column(name = "애니메이션/웹툰")
-    private Integer j;
+    private Integer j=0;
+    @Builder.Default
     @Column(name = "인디음악")
-    private Integer k;
+    private Integer k=0;
+    @Builder.Default
     @Column(name = "일렉트로니카")
-    private Integer l;
+    private Integer l=0;
+    @Builder.Default
     @Column(name = "포크/블루스")
-    private Integer m;
+    private Integer m=0;
 }
