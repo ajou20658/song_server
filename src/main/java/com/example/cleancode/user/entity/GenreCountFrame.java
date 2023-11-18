6ab3181efc6f12d3d9c12251e5be451322e3dd09
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-19 03:11:10 +0900

    Fix: Json객체 수정

--- src/main/java/com/example/cleancode/user/entity/GenreCountFrame.java
+++ src/main/java/com/example/cleancode/user/entity/GenreCountFrame.java
@@ -1,5 +1,6 @@
 package com.example.cleancode.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -11,42 +12,42 @@ import lombok.*;
 @ToString
 public class GenreCountFrame {
     @Builder.Default
-    @Column(name = "J-POP")
+    @JsonProperty("J-POP")
     private Integer a=0;
     @Builder.Default
-    @Column(name = "POP")
+    @JsonProperty("POP")
     private Integer b=0;
     @Builder.Default
-    @Column(name = "R&B/Soul")
+    @JsonProperty("R&B/Soul")
     private Integer c=0;
     @Builder.Default
-    @Column(name = "국내드라마")
+    @JsonProperty("국내드라마")
     private Integer d=0;
     @Builder.Default
-    @Column(name = "댄스")
+    @JsonProperty("댄스")
     private Integer e=0;
     @Builder.Default
-    @Column(name = "랩/힙합")
+    @JsonProperty("랩/힙합")
     private Integer f=0;
     @Builder.Default
-    @Column(name = "록/메탈")
+    @JsonProperty("록/메탈")
     private Integer g=0;
     @Builder.Default
-    @Column(name = "발라드")
+    @JsonProperty("발라드")
     private Integer h=0;
     @Builder.Default
-    @Column(name = "성인가요/트로트")
+    @JsonProperty("성인가요/트로트")
     private Integer i=0;
     @Builder.Default
-    @Column(name = "애니메이션/웹툰")
+    @JsonProperty("애니메이션/웹툰")
     private Integer j=0;
     @Builder.Default
-    @Column(name = "인디음악")
+    @JsonProperty("인디음악")
     private Integer k=0;
     @Builder.Default
-    @Column(name = "일렉트로니카")
+    @JsonProperty("일렉트로니카")
     private Integer l=0;
     @Builder.Default
-    @Column(name = "포크/블루스")
+    @JsonProperty("포크/블루스")
     private Integer m=0;
 }
