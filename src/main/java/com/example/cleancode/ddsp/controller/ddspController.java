bdc3ed4511ed3086123863a2b9169f55742fae69
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-25 16:43:56 +0900

    Fix: 컨트롤러 RestController -> Controller + ResponseBody로 변경

--- src/main/java/com/example/cleancode/ddsp/controller/ddspController.java
+++ src/main/java/com/example/cleancode/ddsp/controller/ddspController.java
@@ -9,6 +9,7 @@ import com.example.cleancode.ddsp.service.InferenceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -16,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/ddsp")
 @RequiredArgsConstructor
 public class ddspController {
@@ -24,6 +25,7 @@ public class ddspController {
     private final PtrDataRepository ptrDataRepository;
     private final InferenceQueue inferenceQueue;
     @GetMapping("/sampleVoiceList")
+    @ResponseBody
     public ResponseEntity<Object> samplePtrList(){
         Map<String,Object> body = new HashMap<>();
         List<PtrData> ptrDataList = ptrDataRepository.findAll();
@@ -31,6 +33,7 @@ public class ddspController {
         return ResponseEntity.ok().body(ptrDataList);
     }
     @GetMapping("/generatedSongList")
+    @ResponseBody
     public List<ResultSong> resultSongList(@RequestParam Long ptrId){
         return inferenceService.allResult(ptrId);
     }
@@ -43,6 +46,7 @@ public class ddspController {
         return ResponseEntity.ok().build();
     }
     @GetMapping("/progressList")
+    @ResponseBody
     public ResponseEntity<Object> showStatus(@RequestParam Long ptrId, @RequestParam Long songId){
         String result = inferenceService.showStatus(ptrId,songId);
         Map<String,Object> body = new HashMap<>();
